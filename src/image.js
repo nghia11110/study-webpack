9fa062f0668b5454b09da42db5af5b4d00bd54f6
NghiaNT <n.nghia@air-closet.com>
2018-09-06 15:42:55 +0900

    add css-loader

---
+++ src/image.js
@@ -0,0 +1,6 @@
+import './image.css'
+
+const image = document.createElement('img');
+image.src = 'http://lorempixel.com/g/400/200/';
+
+document.body.appendChild(image);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import sum from './math'
-// const sum = require('./math');
+// import sum from './math'
+// // const sum = require('./math');
 
-const total = sum(4, 5);
-console.log(total);
-document.write(total);
+// const total = sum(4, 5);
+// console.log(total);
+// document.write(total);
+
+import './image'
